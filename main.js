4e6462b6cd4cf215e813fb816ca9274efa8db256
masupatsela <107406379+masupatsela@users.noreply.github.com>
2022-06-13 15:53:40 +0200

    Updated main.js

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Integer Madness</title>
-    <script src="phaser.in.js"></script>
-  </head>
-  <body>
-    <script type ="module" src="main.js"></script>
-  </body>
-</html>
+export const config = {
+  type: Phaser.AUTO,
+  width: 344,
+  height: 491,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 200
+      },
+      debug: true
+    }
+ }
+ scene: [Game]
+}
+
+new Phaser.Game(config)
+
+
